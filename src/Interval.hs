46416c55def5ebe6ee52046cead7ee8244d90945
Sergey Yavnyi <blacktaxi@gmail.com>
2013-08-20 23:57:42 +0300

    Added intervals.

---
+++ src/Interval.hs
@@ -0,0 +1,18 @@
+module Interval where
+
+data Interval = Interval Integer
+
+p1 = Interval 0
+m2 = Interval 1
+mm2 = Interval 2
+m3 = Interval 3
+mm3 = Interval 4
+p4 = Interval 5
+d5 = Interval 6
+a4 = d5
+p5 = Interval 7
+m6 = Interval 8
+mm6 = Interval 9
+m7 = Interval 10
+mm7 = Interval 11
+p8 = Interval 12
