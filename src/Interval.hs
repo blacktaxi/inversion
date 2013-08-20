597323aa39545c4d680ddf60361de2f3df594b7d
Sergey Yavnyi <blacktaxi@gmail.com>
2013-08-21 00:13:54 +0300

    More interval defs; inversion fn.

--- src/Interval.hs
+++ src/Interval.hs
@@ -1,18 +1,51 @@
 module Interval where
 
 data Interval = Interval Integer
+    deriving (Eq, Ord, Read, Show)
 
-p1 = Interval 0
-m2 = Interval 1
-mm2 = Interval 2
-m3 = Interval 3
-mm3 = Interval 4
-p4 = Interval 5
-d5 = Interval 6
-a4 = d5
-p5 = Interval 7
-m6 = Interval 8
-mm6 = Interval 9
-m7 = Interval 10
-mm7 = Interval 11
-p8 = Interval 12
+perf1 = Interval 0
+dim2 = perf1
+
+min2 = Interval 1
+aug1 = min2
+
+maj2 = Interval 2
+dim3 = maj2
+
+min3 = Interval 3
+aug2 = min3
+
+maj3 = Interval 4
+dim4 = maj3
+
+perf4 = Interval 5
+aug3 = perf4
+
+dim5 = Interval 6
+aug4 = dim5
+
+perf5 = Interval 7
+dim6 = perf5
+
+min6 = Interval 8
+aug5 = min6
+
+maj6 = Interval 9
+dim7 = maj6
+
+min7 = Interval 10
+aug6 = min7
+
+maj7 = Interval 11
+dim8 = maj7
+
+perf8 = Interval 12
+aug7 = perf8
+
+unison = perf1
+semitone = min2
+tone = maj2
+tritone = dim5
+
+inversion :: Interval -> Interval
+inversion (Interval i) = Interval . abs $ i - 12
