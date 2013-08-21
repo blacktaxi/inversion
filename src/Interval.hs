9c96e924d200745e5b0beb60877c2b0306a26fed
Sergey Yavnyi <blacktaxi@gmail.com>
2013-08-21 18:19:54 +0300

    Rearranged definitions.

--- src/Interval.hs
+++ src/Interval.hs
@@ -14,45 +14,37 @@ instance Num Interval where
     -- When does it make sense to multiply intervals?
     (*) = undefined
 
+-- Common intervals
 perf1 = Interval 0
-dim2 = perf1
-
 min2 = Interval 1
-aug1 = min2
-
 maj2 = Interval 2
-dim3 = maj2
-
 min3 = Interval 3
-aug2 = min3
-
 maj3 = Interval 4
-dim4 = maj3
-
 perf4 = Interval 5
-aug3 = perf4
-
 dim5 = Interval 6
-aug4 = dim5
-
 perf5 = Interval 7
-dim6 = perf5
-
 min6 = Interval 8
-aug5 = min6
-
 maj6 = Interval 9
-dim7 = maj6
-
 min7 = Interval 10
-aug6 = min7
-
 maj7 = Interval 11
-dim8 = maj7
-
 perf8 = Interval 12
+
+-- Alternative names
+dim2 = perf1
+aug1 = min2
+dim3 = maj2
+aug2 = min3
+dim4 = maj3
+aug3 = perf4
+aug4 = dim5
+dim6 = perf5
+aug5 = min6
+dim7 = maj6
+aug6 = min7
+dim8 = maj7
 aug7 = perf8
 
+-- 'Human' names
 unison = perf1
 semitone = min2
 tone = maj2
