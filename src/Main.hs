4778808277a247ebaafac65ac7194038b362bd59
Sergey Yavnyi <blacktaxi@gmail.com>
2013-08-24 14:23:01 +0300

    Now use CmdArgs.

--- src/Main.hs
+++ src/Main.hs
@@ -1,38 +1,45 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import System.Environment (getArgs)
-import Control.Monad (mapM)
-import Chord (Chord (..))
+import System.Console.CmdArgs
+import Chord ()
 import Interval (Interval (..))
 import Note (Note (..))
-import Search (chordFingerings, templateChordFingerings)
+import Search (templateChordFingerings)
 import Template (NoteTemplate, ChordTemplate (..), Singleton, TemplateValue)
 import Print (ShowFingering (..))
 import qualified Instrument as I
   
-printChord notes chordNumber instr =
-           putStrLn $ showFingering instr $ chords !! chordNumber
-           where chords = chordFingerings notes instr
-
 deriving instance (Show a, Show b) => Show (ChordTemplate a b)
 deriving instance (Ord a, Read a, Ord b, Read b, TemplateValue a Note, TemplateValue b [Interval]) => Read (ChordTemplate a b)
 
-main = do  
-    (mode:iname:args) <- getArgs
-    let instrument = 
-            case iname of
+data Inversion = Search
+    {instrument :: String
+    ,chord :: String
+    }
+    deriving (Data, Typeable, Show, Eq)
+
+inversion = Search
+    {instrument = def &= opt "guitar" &= help "Instrument to search chords for, 'guitar' or 'ukulele'"
+    ,chord = def &= typ "CHORD DEF" &= help "A chord definition"
+    } &=
+    program "inversion" &=
+    help "Search for chord fingerings" &=
+    summary "Inversion v0.0.1.0, (c) Sergey Yavnyi"
+
+main = do
+    Search{..} <- cmdArgs inversion
+    putStrLn instrument
+    putStrLn chord
+    let instr = 
+            case instrument of
              "ukulele" -> I.ukulele
              "guitar" -> I.guitar
-             _ -> error $ "unknown instrument " ++ iname
+             x -> error $ "unknown instrument " ++ x
         fingerings =
-            case mode of
-            "template" ->
-                let chordTpl = read (unwords args) :: ChordTemplate NoteTemplate (Singleton [Interval])
-                in templateChordFingerings chordTpl instrument
-            "-" ->
-                let (n, o, ints) = read (unwords args)
-                    chord = Chord (Note n o) $ map Interval ints
-                in chordFingerings chord instrument
-    mapM (putStrLn . (showFingering instrument)) fingerings
+            let chordTpl = read chord :: ChordTemplate NoteTemplate (Singleton [Interval])
+            in templateChordFingerings chordTpl instr
+    mapM (putStrLn . (showFingering instr)) fingerings
