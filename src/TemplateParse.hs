89bd5ab4243f38eb4573f0b8cf69c1c17436475b
Sergey Yavnyi <blacktaxi@gmail.com>
2013-08-25 20:42:25 +0300

    Convert tabs to spaces, etc.

--- src/TemplateParse.hs
+++ src/TemplateParse.hs
@@ -1,7 +1,6 @@
 module TemplateParse (parseChordTemplate) where
 
 import Text.ParserCombinators.Parsec
-import Data.Maybe (maybeToList)
 import Control.Applicative ((<$>), (*>), (<*))
 
 import Note (ABC (..), Octave (..))
@@ -15,37 +14,37 @@ import Template
 -- <chord_quality> :: "m" | "M" | ""
 -- <interval_num> :: "(" ("maj" | "min" | "aug" | "dim")<number> ")"
 
-chordTemplate :: CharParser () (ChordTemplate NoteTemplate [Interval])
-chordTemplate =	do
-	note' <- templateValue note
-	octave' <- (maybe Any id) <$> optionMaybe (char '(' *> templateValue octave <* char ')')
-	intervals' <- intervals
-	return $ ChordTemplate (NoteTemplate note' octave') intervals'
+chord :: CharParser () (ChordTemplate NoteTemplate [Interval])
+chord = do
+    note' <- templateValue note
+    octave' <- (maybe Any id) <$> optionMaybe (char '(' *> templateValue octave <* char ')')
+    intervals' <- intervals
+    return $ ChordTemplate (NoteTemplate note' octave') intervals'
 
 note :: CharParser () ABC
 note = do
-	n <- oneOf "ABCDEFG"
-	m <- maybe [] ((: []) . sharpOrFlat) <$> optionMaybe (oneOf "#b")
-	return $ read ([n] ++ m)
-	where 
-		sharpOrFlat '#' = 's'
-		sharpOrFlat x = x
+    n <- oneOf "ABCDEFG"
+    m <- maybe [] ((: []) . sharpOrFlat) <$> optionMaybe (oneOf "#b")
+    return $ read ([n] ++ m)
+    where 
+        sharpOrFlat '#' = 's'
+        sharpOrFlat x = x
 
 octave :: CharParser () Octave
 octave = Octave . read . (: []) <$> oneOf "012345678"
 
 intervals :: CharParser () [Interval]
 intervals = map (Interval . read) <$> 
-	(char '{' *> sepBy1 (many1 digit) (char ',') <* char '}')
+    (char '{' *> sepBy1 (many1 digit) (char ',') <* char '}')
 
 templateValue :: CharParser () a -> CharParser () (TemplateOption a)
 templateValue p = (maybe Any OneOf) <$> (optionMaybe oneOrManyV)
-	where 
-		oneV = (: []) <$> p
-		manyV = char '[' *> sepBy1 p (char ',') <* char ']'
-		oneOrManyV = manyV <|> oneV
+    where 
+        oneV = (: []) <$> p
+        manyV = char '[' *> sepBy1 p (char ',') <* char ']'
+        oneOrManyV = manyV <|> oneV
 
 parseChordTemplate :: String 
-	-> String 
-	-> Either ParseError (ChordTemplate NoteTemplate [Interval])
-parseChordTemplate = parse chordTemplate
+    -> String 
+    -> Either ParseError (ChordTemplate NoteTemplate [Interval])
+parseChordTemplate = parse chord
