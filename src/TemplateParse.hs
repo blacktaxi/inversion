7e0847d2ee5df7f5bd6cccef4c4e4af99795bb76
Sergey Yavnyi <blacktaxi@gmail.com>
2013-09-08 17:16:28 +0300

    Added literal chord spec parsing.

--- src/TemplateParse.hs
+++ src/TemplateParse.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE RecordWildCards #-}
 module TemplateParse (parseChordTemplate) where
 
 import Text.ParserCombinators.Parsec
-import Control.Applicative ((<$>), (*>), (<*), (<$))
-import Data.Maybe (fromMaybe, isJust)
+import Control.Applicative ((<$>), (*>), (<*), (<$), (<*>))
+import Data.Maybe (fromMaybe, isJust, catMaybes)
 
 import Note (ABC (..), Octave (..))
 import Interval (Interval (..))
@@ -31,9 +32,10 @@ pChord :: CharParser () (ChordPattern NotePattern [IntervalPattern])
 pChord = do
     note <- "note pattern" <??> templateValue pNote
     octave <- "octave pattern" <??> fromMaybe Any <$> 
-        optionMaybe (char '(' *> templateValue pOctave <* char ')')
+        optionMaybe (char '<' *> templateValue pOctave <* char '>')
     --intervals' <- specIntervals <|> explicitIntervals
-    intervalPatterns <- "chord spec" <??> pExplicitIntervals
+    intervalPatterns <- "chord spec" <??>
+        pExplicitIntervals <|> (chordSpecToIntervals <$> pChordSpec)
     return $ ChordPattern (NotePattern note octave) intervalPatterns
 
 pNote :: CharParser () ABC
@@ -62,7 +64,12 @@ pIntervalName =  "interval name" <??> choiceInterval
     ,(In.maj6, ["M6", "maj6", "d7", "dim7"])
     ,(In.min7, ["m7", "min7", "A6", "aug6"])
     ,(In.maj7, ["M7", "maj7", "d8", "dim8"])
-    ,(In.perf8, ["P8", "perf8", "A7", "aug7"])]
+    ,(In.perf8, ["P8", "perf8", "A7", "aug7"])
+    ,(In.Interval 13, ["m9", "min9"]) -- p8 + min2
+    ,(In.Interval 14, ["M9", "maj9"]) -- p8 + maj2
+    ,(In.Interval 15, ["m10", "min10"]) -- p8 + min3
+    ,(In.Interval 16, ["M10", "maj10"]) -- p8 + maj3
+    ]
     where
         p_oneName (interval, names) = try $ interval <$ oneOfStr names
         choiceInterval is = choice (map p_oneName is)
@@ -85,7 +92,8 @@ pIntervalPattern = do
 pExplicitIntervals :: CharParser () [IntervalPattern]
 pExplicitIntervals = char '{' *> sepBy1 pIntervalPattern (char ',') <* char '}'
 
-templateValue :: (Bounded a, Enum a) => CharParser () a -> CharParser () (PatternValue a)
+templateValue :: (Bounded a, Enum a) =>
+    CharParser () a -> CharParser () (PatternValue a)
 templateValue p =
     Any <$ char '*' <|>
     maybe Any OneOf <$> optionMaybe oneOrManyV
@@ -93,6 +101,78 @@ templateValue p =
         oneV = (: []) <$> p
         manyV = "pattern" <??> char '[' *> sepBy1 p (char ',') <* char ']'
         oneOrManyV = manyV <|> oneV
+
+data ChordQuality = MinorChord | MajorChord
+data IntervalQuality = Minor | Major | Diminished | Augmented | Dominant
+data ChordSpec = ChordSpec 
+    { chordQuality :: Maybe ChordQuality,
+    mainInterval :: Maybe (Maybe IntervalQuality, Integer),
+    addInterval :: Maybe Integer
+    }
+
+pChordSpec :: CharParser () ChordSpec
+pChordSpec =
+    createSpec <$> pChordQuality <*> pIntervalSpec <*> pAdditionalInterval
+    where
+        createSpec q mi ai =
+            ChordSpec { chordQuality = q, mainInterval = mi, addInterval = ai }
+        pChordQuality =
+            optionMaybe
+                ((try $ MinorChord <$ char 'm') <|>
+                (try $ MajorChord <$ char 'M'))
+        pIntervalQuality =
+            optionMaybe $
+                choice (map try 
+                [Minor <$ (string "min")
+                ,Major <$ (string "maj")
+                ,Diminished <$ (string "dim")
+                ,Augmented <$ (string "aug")
+                ,Dominant <$ (string "dom")])
+        pIntervalNumber = read <$> many1 digit
+        pAdditionalInterval =
+            optionMaybe $
+                read <$> (string "add" *> many1 digit)
+        pIntervalSpec =
+            optionMaybe $
+                (,) <$> (char '(' *> pIntervalQuality) <*> (pIntervalNumber <* char ')')
+
+chordSpecToIntervals :: ChordSpec -> [IntervalPattern]
+chordSpecToIntervals (ChordSpec {..}) =
+    root : (catMaybes $ third : fifth : main : added : [])
+    where
+        ipat x y = IntervalPatternValue { interval = x, inversionsAllowed = y }
+        inversions x = ipat x True
+        noInversions x = ipat x False
+        root = ExactlyOne $ noInversions (In.Interval 0)
+
+        chordQ = fromMaybe MajorChord chordQuality
+        -- @TODO ugly stuff
+        makeInterval q n =
+            In.Interval $ neutralI + modI
+            where
+                neutralI =
+                    case n of
+                        7 -> 11
+                        6 -> 9
+                        9 -> 14
+                        10 -> 16
+                        11 -> 17 -- ??
+                modI =
+                    case q of
+                        Major -> 0
+                        Minor -> -1
+                        Augmented -> 1
+                        Diminished -> -2
+                        Dominant -> error "ugh"
+
+        third = Just . ExactlyOne . noInversions $
+            (case chordQ of
+                MajorChord -> In.maj3
+                MinorChord -> In.min3)
+        fifth = Just . ExactlyOne . inversions $ In.perf5
+        main = ExactlyOne . inversions . (\(quality, num) -> makeInterval (fromMaybe Major quality) num) <$>
+            mainInterval
+        added = Nothing
 
 parseChordTemplate :: String 
     -> Either ParseError (ChordPattern NotePattern [IntervalPattern])
